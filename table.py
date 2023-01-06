a10f30001d035832d011c77911487c6cafed1cc6
anishaw <119960150+anishaw@users.noreply.github.com>
2023-01-06 11:40:54 -0600

    Create table.py

---
+++ table.py
@@ -0,0 +1,28 @@
+google-cloud-bigtable==2.14.1
+google-cloud-core==2.3.2
+
+#Import the modules
+import datetime
+
+from google.cloud import bigtable
+from google.cloud.bigtable import column_family
+from google.cloud.bigtable import row_filters
+
+# The client must be created with admin=True because it will create a
+# table.
+client = bigtable.Client(project=project_id, admin=True)
+instance = client.instance(instance_id)
+
+print("Creating the {} table.".format(table_id))
+table = instance.table(table_id)
+
+print("Creating column family cf1 with Max Version GC rule...")
+# Create a column family with GC policy : most recent N versions
+# Define the GC policy to retain only the most recent 2 versions
+max_versions_rule = column_family.MaxVersionsGCRule(2)
+column_family_id = "cf1"
+column_families = {column_family_id: max_versions_rule}
+if not table.exists():
+    table.create(column_families=column_families)
+else:
+    print("Table {} already exists.".format(table_id))
