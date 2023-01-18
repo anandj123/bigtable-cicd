81179a55cc72c51d27758370ee8adf84df5ee833
anishaw <anishaw@google.com>
2023-01-18 12:45:47 -0600

    organized

--- source/apply_schema.py
+++ source/apply_schema.py
@@ -1,18 +1,29 @@
-"""Demonstrates how to connect to Cloud Bigtable and run some basic operations.
-Prerequisites:
-- Create a Cloud Bigtable cluster.
-  https://cloud.google.com/bigtable/docs/creating-cluster
-- Set your Google Application Default Credentials.
-  https://developers.google.com/identity/protocols/application-default-credentials
-"""
+#!/usr/bin/python
+#
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#Description
+
 
 #Import the modules
 import datetime
 import os
+import yaml
 from google.cloud import bigtable
 from google.cloud.bigtable import column_family
 from google.cloud.bigtable import row_filters
-import yaml
 from yaml.loader import SafeLoader
 from google.cloud.bigtable import Client
 from google.cloud.bigtable import enums
